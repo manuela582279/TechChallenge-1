1f5da7bcc991f9d69bd896d86206a4bf11037c40
Gabi08-I <senaioloko@gmail.com>
2026-07-23 16:20:33 -0300

    primeiro commit - project TechChallenge

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TechChallenge.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
