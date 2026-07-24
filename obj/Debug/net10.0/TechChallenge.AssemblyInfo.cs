cadfbde8d3d852426497f5ccc68f01f1f69b62b2
Gabi08-I <senaioloko@gmail.com>
2026-07-24 09:16:51 -0300

    Desafio M01: Customizar página inicial do TechChallenge

--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5da7bcc991f9d69bd896d86206a4bf11037c40")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
