76c89c45e6d3de8091e36518558d73a87bd3885e
Gabi08-I <senaioloko@gmail.com>
2026-07-30 10:10:54 -0300

    Atualiza versão informacional do assembly para 1.0.0+46a99bddf03702d1bbe7c94ea20cbf21c9f9d3fc

--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9f56acf19182242127c2400925a68d6d751d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a99bddf03702d1bbe7c94ea20cbf21c9f9d3fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
