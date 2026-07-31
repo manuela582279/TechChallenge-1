10322118207f7fac90cdc80cd65a5ce4c4ac0738
Manuela582279 <manuela.domingues@aluno.senai.br>
2026-07-31 16:30:36 -0300

    M05: Relacionamentos entre entidades

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,15 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using TechChallenge.Models;
 
 namespace TechChallenge;
 
-public class AppDbContext : DbContext //Herança do Contexto do EF Core 
+public class AppDbContext : DbContext  // Herança do Contexto do EF Core
 {
-    //Herança no Método Construtor
+    // Herança no Método Construtor
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    {}
+    /*
+        DbSet -> Representação da Tabela no Sistema.
+        Para cada model do sistema que for virar uma tabela no Banco de
+        de Dados, deverá ser adicionado um DbSet
+    */
+    public DbSet<Aluno> Alunos {get; set;}  
+    public DbSet<Categoria> Categorias {get; set;} 
+    public DbSet<Professor> Professores { get; set; }
+    public DbSet<Equipe> Equipes { get; set; }
+    public DbSet<AlunoEquipe> AlunosEquipes { get; set; }
+    public DbSet<Projeto> Projetos { get; set; }
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+   	 {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<AlunoEquipe>().HasKey(ae => new { ae.AlunoId, ae.EquipeId });
 
-    // DbSet -> Representação da Tabela no Sistema. Para cada model do sistema que for virar uma tabela no Banco de de Dados, deverá ser adicionado um DbSet
-    public DbSet <Aluno> Alunos {get; set;}
-    public DbSet <Categoria> Categorias {get; set;}
+    	}
+
 }
+
+
--- obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207fd8b961e3577b738d13af0e669d09f19289f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be728918ffea6ab07d67970d4db00e27804fb442")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
